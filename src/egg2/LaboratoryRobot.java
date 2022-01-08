337f2abd66b709e79aa0f936eae4df3dce2fb20f
AnOvercookedFork <oldamazingbrain@gmail.com>
2022-01-07 22:22:14 -0500

    added lab

--- src/egg2/LaboratoryRobot.java
+++ src/egg2/LaboratoryRobot.java
@@ -3,6 +3,8 @@ package egg2;
 import battlecode.common.*;
 
 public strictfp class LaboratoryRobot extends Robot {
+    private static final RATIO = 0.25;
+    private static final MIN_LEAD = 100;
 
     public LaboratoryRobot(RobotController rc) {
         super(rc);
@@ -10,6 +12,22 @@ public strictfp class LaboratoryRobot extends Robot {
 
     @Override
     public void run() throws GameActionException {
-        //TODO
+        processNearbyRobots();
+
+        tryTransmute();
+    }
+
+    RobotInfo[] nearbyRobots;
+    public void processNearbyRobots() throws GameActionException {
+        nearbyRobots = rc.senseNearbyRobots();
+        processAndBroadcastEnemies(nearbyRobots);
+    }
+
+    public void tryTransmute() throws GameActionException {
+        int lead = 	getTeamLeadAmount(rc.getTeam());
+        int gold = 	getTeamGoldAmount(rc.getTeam());
+        if(rc.canTransmute() && lead >= MIN_LEAD && (gold / lead) <= RATIO) {
+            rc.transmute();
+        }
     }
 }
