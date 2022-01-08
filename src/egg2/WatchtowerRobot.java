4694b8f81848ee484c176f045b8a95a3daa83132
AnOvercookedFork <oldamazingbrain@gmail.com>
2022-01-07 21:25:43 -0500

    added basic watchtower - can upgrade targetting later

--- src/egg2/WatchtowerRobot.java
+++ src/egg2/WatchtowerRobot.java
@@ -10,6 +10,64 @@ public strictfp class WatchtowerRobot extends Robot {
 
     @Override
     public void run() throws GameActionException {
-        //TODO
+        processNearbyRobots();
+        tryAttack();
+    }
+
+    RobotInfo[] nearbyRobots;
+    boolean areEnemiesNearby;
+    public void processNearbyRobots() throws GameActionException {
+        nearbyRobots = rc.senseNearbyRobots();
+        for (RobotInfo otherRobot : nearbyRobots) {
+            if (otherRobot.team == rc.getTeam()) {
+
+            } else {
+                areEnemiesNearby = true;
+                break;
+            }
+        }
+        processAndBroadcastEnemies(nearbyRobots);
+    }
+
+    public boolean tryAttack() throws GameActionException {
+        if (rc.isActionReady()) {
+            MapLocation target = identifyTarget();
+            if (target != null && rc.canAttack(target)) {
+                rc.attack(target);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Targets the opposing enemy with the lowest health, prioritizing units that can attack;
+     * returns null if no such enemy is found.
+     **/
+    public MapLocation identifyTarget() throws GameActionException {
+        if (!areEnemiesNearby) return null;
+        MapLocation best = null;
+        int health = Integer.MAX_VALUE;
+        boolean canAttack = false;
+        for (RobotInfo otherRobot : nearbyRobots) {
+            if (otherRobot.team != rc.getTeam()
+                    && rc.getLocation().isWithinDistanceSquared(otherRobot.location, RobotType.SOLDIER.actionRadiusSquared)) {
+                if (otherRobot.type.canAttack()
+                        && otherRobot.mode.canAct
+                        && rc.getLocation().isWithinDistanceSquared(otherRobot.location, otherRobot.type.actionRadiusSquared)) {
+                    if (otherRobot.health < health) {
+                        health = otherRobot.health;
+                        canAttack = true;
+                        best = otherRobot.location;
+                    }
+                } else if (!canAttack) {
+                    if (otherRobot.health < health) {
+                        health = otherRobot.health;
+                        best = otherRobot.location;
+                    }
+                }
+            }
+        }
+        return best;
     }
 }
