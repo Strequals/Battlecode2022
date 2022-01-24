ee5f7f3d191021535f61ee20fd66e2294e414dce
AnOvercookedFork <oldamazingbrain@gmail.com>
2022-01-24 13:51:45 -0500

    made copy of bot four

---
+++ src/four/RobotPlayer.java
@@ -0,0 +1,48 @@
+package four;
+
+import battlecode.common.*;
+
+public strictfp class RobotPlayer {
+
+    static int turnCount = 0;
+
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+
+        Robot robot = null;
+
+        switch (rc.getType()) {
+            case ARCHON:
+                robot = new ArchonRobot(rc); break;
+            case MINER:
+                robot = new MinerRobot(rc); break;
+            case SOLDIER:
+                robot = new SoldierRobot(rc); break;
+            case LABORATORY:
+                robot = new LaboratoryRobot(rc); break;
+            case WATCHTOWER:
+                robot = new WatchtowerRobot(rc); break;
+            case BUILDER:
+                robot = new BuilderRobot(rc); break;
+            case SAGE:
+                robot = new SageRobot(rc); break;
+        }
+
+        while (true) {
+
+            turnCount += 1;
+
+            try {
+                robot.run();
+            } catch (GameActionException e) {
+                System.out.println(rc.getType() + " Exception");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.out.println(rc.getType() + " Exception");
+                e.printStackTrace();
+            } finally {
+                Clock.yield();
+            }
+        }
+    }
+}
